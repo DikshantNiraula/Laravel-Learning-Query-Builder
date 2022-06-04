55ced95f1eee6c68c1e85dea3bfe0a99c570a753
Dikshant Niraula <dikshantniraula351@gmail.com>
2022-06-04 18:23:18 +0545

    Category and Product Model Class Updated

--- database/migrations/2022_06_04_122506_create_products_table.php
+++ database/migrations/2022_06_04_122506_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('price');
+            $table->string('stock');
+            $table->string('category_id');
             $table->timestamps();
         });
     }
